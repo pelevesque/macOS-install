b04a4895bfef9c459be1671548cb8e1aaa50bec7
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2022-10-21 12:05:06 -0400

    Add features to init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,11 +2,24 @@ local o = vim.opt
 
 o.number = true
 o.wrap = true
+o.colorcolumn = '70,80'
 
 require ('packer').startup(function()
     use 'wbthomason/packer.nvim'
     use 'navarasu/onedark.nvim'
+    use 'airblade/vim-gitgutter'
+
+    -- lualine needs patched fonts installed.
+    -- https://www.nerdfonts.com
+    use 'nvim-lualine/lualine.nvim'
 end)
 
 require('onedark').setup { style = 'warmer' }
 require('onedark').load()
+
+require('lualine').setup()
+
+vim.api.nvim_exec([[
+    hi EOLSpace ctermbg=238 guibg=#333333
+    match EOLSpace /\s\+$/
+]], false)
