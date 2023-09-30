06e5596f6206beb1e1cb30e22b99178d5e8ef87e
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2023-09-30 03:42:14 -0400

    Update NeoVim config

--- configurations/dotfiles/.config/nvim/init.lua
+++ configurations/dotfiles/.config/nvim/init.lua
@@ -1,26 +1,57 @@
 local o = vim.opt
 
 o.number = true
-o.wrap = true
+o.relativenumber = true
 o.colorcolumn = '70,80'
-o.expandtab = true
+o.wildmenu = true
 
-require ('packer').startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'navarasu/onedark.nvim'
-    use 'airblade/vim-gitgutter'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
 
-    -- lualine needs patched fonts installed.
-    -- https://www.nerdfonts.com
-    use 'nvim-lualine/lualine.nvim'
-end)
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "navarasu/onedark.nvim",
+    "airblade/vim-gitgutter",
+})
 
 require('onedark').setup { style = 'warmer' }
 require('onedark').load()
-
-require('lualine').setup()
 
 vim.api.nvim_exec([[
     hi EOLSpace ctermbg=238 guibg=#333333
     match EOLSpace /\s\+$/
 ]], false)
+
+local function notEmpty(s) return s ~= nil and s ~= '' end
+
+local function varProfiler(lang, output)
+    local vars
+    vim.ui.input(
+        { prompt = string.format('Enter %s var name(s): ', lang) },
+        function(input) vars = input end
+    )
+    if notEmpty(vars) then
+        for var in vars:gmatch('%S+') do
+            local line = string.format(output, var, var)
+            vim.api.nvim_put({ line }, 'l', false, true)
+        end
+    end
+end
+
+vim.keymap.set('n', 'Km',
+    function() varProfiler('JS', "console.log('%s: ' + %s);") end
+)
+
+vim.keymap.set('n', 'Kn',
+    function() varProfiler('Raku', 'note("%s: <$%s>");') end
+)
