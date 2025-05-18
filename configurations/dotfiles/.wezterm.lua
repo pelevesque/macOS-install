d94dfc3e0b37be5d1fa1de2dd4a558023f6257dd
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2025-05-17 20:27:39 -0400

    Wezterm: Update config

--- configurations/dotfiles/.wezterm.lua
+++ configurations/dotfiles/.wezterm.lua
@@ -4,9 +4,22 @@ local c = wezterm.config_builder()
 c.initial_cols = 80
 c.initial_rows = 40
 
-c.font_size = 20
 c.font= wezterm.font 'IBM PLEX MONO'
+c.font_size = 20
 
-c.color_scheme = 'AdventureTime'
+c.color_scheme = 'Catppuccin Macchiato'
+
+    -- Removes Apple window resizing circles.
+c.window_decorations = 'RESIZE'
+
+-- c.enable_tab_bar = false
+
+c.window_background_opacity = 0.97
+c.macos_window_background_blur = 90
+
+c.keys = {
+        -- macOs style fullscreen toggle for system consistency.
+    { key = 'f', mods = 'CTRL|CMD', action = wezterm.action.ToggleFullScreen },
+}
 
 return c
